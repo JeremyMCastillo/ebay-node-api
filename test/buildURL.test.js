8e1966c24c47e4a8bb6abfd788e110a8cc16b3df
Jeremy Castillo <jeremy.c@accessorypower.com>
2019-06-03 16:02:54 -0700

    Formatting and fixing buildURL tests.
    Signed-off-by: Jeremy Castillo <jeremy.c@accessorypower.com>

--- test/buildURL.test.js
+++ test/buildURL.test.js
@@ -1,47 +1,44 @@
-let expect = require("chai").expect;
-let should = require('chai').should();
-let eBay = require('../src/index');
-let buildURL = require('../src/buildURL');
+const { expect } = require('chai');
+require('chai').should();
+require('../src/index');
+const buildURL = require('../src/buildURL');
 
-describe("test building url methods", () => {
+describe('test building url methods', () => {
+  it('test search url', () => {
+    const expectedSearchUrl = 'https://svcs.ebay.com/services/search/FindingService/v1?SECURITY-APPNAME=testID&OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&RESPONSE-DATA-FORMAT=JSON&keywords=iphone&paginationInput.entriesPerPage=6&GLOBAL-ID=EBAY-US';
+    const options = {
+      keywords: 'iphone',
+      operationName: 'findItemsByKeywords',
+      clientID: 'testID',
+      limit: 6,
+      globalID: 'EBAY-US',
+      baseSvcUrl: 'svcs.ebay.com'
+    };
+    expect(buildURL.buildSearchUrl(options)).to.be.equal(expectedSearchUrl);
+  });
 
+  it('test Shopping url without selector', () => {
+    const expectedSearchUrl = 'https://open.api.ebay.com/Shopping?appid=testID&callname=demoShoppingName&version=967&siteid=0&responseencoding=JSON&keywords=iphone';
+    const options = {
+      name: 'iphone',
+      operationName: 'demoShoppingName',
+      param: 'keywords',
+      clientID: 'testID',
+      baseUrl: 'open.api.ebay.com'
+    };
+    expect(buildURL.buildShoppingUrl(options)).to.be.equal(expectedSearchUrl);
+  });
 
-    it("test search url", () => {
-        let expected_search_url = "https://svcs.ebay.com/services/search/FindingService/v1?SECURITY-APPNAME=testID&OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&RESPONSE-DATA-FORMAT=JSON&keywords=iphone&paginationInput.entriesPerPage=6&GLOBAL-ID=EBAY-US"
-        let options = {
-            name: "iphone",
-            operationName: "findItemsByKeywords",
-            param: "keywords",
-            clientID: "testID",
-            limit: 6,
-            globalID: "EBAY-US",
-            baseSvcUrl: 'svcs.ebay.com'
-        }
-        expect(buildURL.buildSearchUrl(options)).to.be.equal(expected_search_url);
-    });
-
-    it("test Shopping url without selector", () => {
-        let expected_search_url = "https://open.api.ebay.com/Shopping?appid=testID&callname=demoShoppingName&version=967&siteid=0&responseencoding=JSON&keywords=iphone"
-        let options = {
-            name: "iphone",
-            operationName: "demoShoppingName",
-            param: "keywords",
-            clientID: "testID",
-            baseUrl: 'open.api.ebay.com'
-        }
-        expect(buildURL.buildShoppingUrl(options)).to.be.equal(expected_search_url);
-    });
-
-    it("test Shopping url including selector", () => {
-        let expected_search_url = "https://open.api.ebay.com/Shopping?appid=testID&callname=demoShoppingName&version=967&siteid=0&responseencoding=JSON&keywords=iphone&IncludeSelector=true"
-        let options = {
-            name: "iphone",
-            operationName: "demoShoppingName",
-            param: "keywords",
-            clientID: "testID",
-            includeSelector: true,
-            baseUrl: 'open.api.ebay.com'
-        }
-        expect(buildURL.buildShoppingUrl(options)).to.be.equal(expected_search_url);
-    });
+  it('test Shopping url including selector', () => {
+    const expectedSearchUrl = 'https://open.api.ebay.com/Shopping?appid=testID&callname=demoShoppingName&version=967&siteid=0&responseencoding=JSON&keywords=iphone&IncludeSelector=true';
+    const options = {
+      name: 'iphone',
+      operationName: 'demoShoppingName',
+      param: 'keywords',
+      clientID: 'testID',
+      includeSelector: true,
+      baseUrl: 'open.api.ebay.com'
+    };
+    expect(buildURL.buildShoppingUrl(options)).to.be.equal(expectedSearchUrl);
+  });
 });
